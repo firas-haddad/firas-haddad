7451af36055dba09eb4bd242bd0c68e5212dd6bf
firas-haddad <kobani64444@gmail.com>
2026-02-23 22:23:11 +0300

    Update ga.js

--- js/ga.js
+++ js/ga.js
@@ -1,1 +1,8 @@
-directly to the main branch
+<!-- Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-L04TVV5R1T"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-L04TVV5R1T');
+</script>directly to the main branch
